b64a03793ece8887296541c64d0042051faf2278
Rancher-V <yangmingxiework@gmail.com>
2017-11-19 17:58:48 -0500

    coordinate with bigtable

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
         "reflect"
         "gopkg.in/olivere/elastic.v3"
         "github.com/pborman/uuid"
+        "context"
+        "cloud.google.com/go/bigtable"
         "strings"
 )
 
@@ -17,8 +19,8 @@ const (
         TYPE = "post"
         DISTANCE = "200km"
         // Needs to update
-        //PROJECT_ID = "around-xxx"
-        //BT_INSTANCE = "around-post"
+        PROJECT_ID = "around-xxx"
+        BT_INSTANCE = "around-post"
         // Needs to update this URL if you deploy it to cloud.
         ES_URL = "http://35.185.108.84:9200"
 )
@@ -142,12 +144,7 @@ func handlerSearch(w http.ResponseWriter, r *http.Request) {
         w.Header().Set("Content-Type", "application/json")
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Write(js)
-
-
-
 }
-
-
 
 func handlerPost(w http.ResponseWriter, r *http.Request) {
         // Parse from body of request to get a json object.
@@ -162,7 +159,43 @@ func handlerPost(w http.ResponseWriter, r *http.Request) {
         id := uuid.New()
         // Save to ES.
         saveToES(&p, id)
+
+
+        fmt.Printf( "Post is saved to Index: %s\n", p.Message)
+
+        ctx := context.Background()
+        // you must update project name here
+        bt_client, err := bigtable.NewClient(ctx, PROJECT_ID, BT_INSTANCE)
+        if err != nil {
+                panic(err)
+                return
+        }
+
+        tbl := bt_client.Open("post")
+        mut := bigtable.NewMutation()
+        t := bigtable.Now()
+
+        mut.Set("post", "user", t, []byte(p.User))
+        mut.Set("post", "message", t, []byte(p.Message))
+        mut.Set("location", "lat", t, []byte(strconv.FormatFloat(p.Location.Lat, 'f', -1, 64)))
+        mut.Set("location", "lon", t, []byte(strconv.FormatFloat(p.Location.Lon, 'f', -1, 64)))
+
+        err = tbl.Apply(ctx, id, mut)
+        if err != nil {
+                panic(err)
+                return
+        }
+        fmt.Printf("Post is saved to BigTable: %s\n", p.Message)
+
+
+
 }
+
+
+
+
+
+
 
 
 func saveToES(p *Post, id string) {
